2660ae805e0ad20b09f2d36ce59b78b70b7ac86f
ivey <iveysaurrr@gmail.com>
2014-08-05 22:21:47 -0400

    No hard coded file locations. Fix #1

--- uptime.php
+++ uptime.php
@@ -1,7 +1,7 @@
 <?php
 sleep(rand(0,220));
 
-$uptime_big = file_get_contents("/home/jonathan/uptime/uptime.txt");
+$uptime_big = file_get_contents(__DIR__ . "/uptime.txt");
 
 $urls = explode("\n", $uptime_big);
 
@@ -9,6 +9,6 @@ foreach ($urls as $url) {
 	if ($url == "") continue;
 	$d = file_get_contents($url);
 	if (!$d) {
-//		system("/usr/local/bin/node /home/jonathan/uptime/warn.js");
+//		system("/usr/local/bin/node " . __DIR__ ."/warn.js");
 	}
 }
